40e9df0fa6bc953dced168b1d9df30d6b28db9a0
Felicia Davis <davisfeliciam@gmail.com>
2019-02-07 20:23:50 -0800

    fixed function name.......

--- divisibleBy13.js
+++ divisibleBy13.js
@@ -2,7 +2,7 @@
  * Search array for numbers divisible by 13
  * @param {Array<number>} nums
  */
-function countEvens(nums) {
+function countMultiplesOf13(nums) {
     let totalEvens = 0;
     for (const num of nums) {
         if (num % 13 === 0){
@@ -11,6 +11,6 @@ function countEvens(nums) {
     }
     return totalEvens;
 }
-let nums = countEvens([13, 26, 39, 49, 56, 62, 74, 85, 99, 100]);
+let nums = countMultiplesOf13([13, 26, 39, 49, 56, 62, 74, 85, 99, 100]);
 console.log(nums);
 
