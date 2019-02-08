be6ff1d4211f8108c896efaf5fdbc21186b390ad
Felicia Davis <davisfeliciam@gmail.com>
2019-02-07 20:13:57 -0800

    fixed loop

--- countEvens.js
+++ countEvens.js
@@ -5,11 +5,12 @@
 function countEvens(nums) {
     let totalEvens = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0){
+        let num = nums[i];
+        if (num % 2 === 0){
             totalEvens += 1;
         }
     }
     return totalEvens;
 }
-let nums = countEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+let nums = countEvens([2, 4, 6, 8, 10]);
 console.log(nums);
