bf760c34c9b27ad79a3e30aa8defbe1d091936b7
Felicia Davis <davisfeliciam@gmail.com>
2019-02-08 16:45:26 -0800

    Day 3 problem

---
+++ countWords.js
@@ -0,0 +1,16 @@
+/**
+ * print how many wors are in a given string of text
+ * @param {string} str
+ */
+function countWords(str) {
+    let totalWords = 0;
+    for (const word of str) {
+        if (word === ' ') {
+            totalWords += 1;
+        }
+    }
+    return totalWords +1;
+}
+
+let sentence = countWords('How many words are in this sentence?');
+console.log(sentence);
