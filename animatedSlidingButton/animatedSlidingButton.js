b08680bab3ba92681f810d4c723f6681e1ad9afb
Felicia Davis <davisfeliciam@gmail.com>
2019-02-16 20:57:17 -0800

    Day 11 problem

---
+++ animatedSlidingButton/animatedSlidingButton.js
@@ -0,0 +1,12 @@
+/**
+ * Make page with a button that slides when clicked
+ */
+
+const button = document.querySelector('button');
+button.addEventListener('click', function () {
+    const left = Math.floor(Math.random() * 500);
+    const top = Math.floor(Math.random() * 500);
+    button.style.left = `${left}px`;
+    button.style.top = `${top}px`;
+
+});
