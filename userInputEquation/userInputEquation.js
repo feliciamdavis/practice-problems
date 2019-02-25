d8f9795040bcebb16efa104a812b4c633c74a85b
Felicia Davis <davisfeliciam@gmail.com>
2019-02-24 23:38:24 -0500

    Day 19 problem

---
+++ userInputEquation/userInputEquation.js
@@ -0,0 +1,31 @@
+const number1El = document.getElementById('number1');
+const number2El = document.getElementById('number2');
+const totalEl = document.getElementById('total');
+const operatorStringEl = document.getElementById('operator');
+
+function getEquationAnswer(){
+    const number1String = number1El.value;
+    const number2String = number2El.value;
+    const operatorString = operatorStringEl.value;
+    const number1 = Number.parseInt(number1String);
+    const number2 = Number.parseInt(number2String);
+
+    let total;
+    if (operatorString === '+') {
+        total = number1 + number2;
+    }
+    else if (operatorString === '-') {
+        total = number1 - number2;
+    }
+    else if (operatorString === '*') {
+        total = number1 * number2;
+    }
+    else if (operatorString === '/') {
+        total = number1 / number2;
+    }
+
+    totalEl.innerHTML = total;
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', getEquationAnswer);
