eec19c3e9d3ccf9b00df1d12b0a3133e1a3ccd7b
Felicia Davis <davisfeliciam@gmail.com>
2019-03-04 01:31:26 -0500

    Webified

--- checkPalindrom/checkPalindrome.js
+++ checkPalindrom/checkPalindrome.js
@@ -3,7 +3,7 @@
  * ('mom', '01234543210', 'A Man, A Plan, A Canal: Panama!')
  * @param {string} str
  */
-function isPalindrome(str) {
+function getIsPalindrome(str) {
     const fixedString = str.replace(/[.?!, :-;]/g, '').toLowerCase();
     for (let firstIdx = 0; firstIdx < fixedString.length; firstIdx++) {
         const lastIdx = fixedString.length - firstIdx - 1;
@@ -16,4 +16,12 @@ function isPalindrome(str) {
     return true;
 }
 
-console.log(isPalindrome('A Man, A Plan, A Canal: Panama!'));
+const sentenceEl = document.getElementById('sentence');
+const answerEl = document.getElementById('answer');
+
+const buttonEl = document.querySelector('button');
+buttonEl.addEventListener('click', function() {
+    const sentence = sentenceEl.value;
+    const isPalindrome = getIsPalindrome(sentence);
+    answerEl.innerHTML = isPalindrome;
+});
