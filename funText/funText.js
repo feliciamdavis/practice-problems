812943ad052e2049bfec2fdfc48193dfa57b8ccf
Felicia Davis <davisfeliciam@gmail.com>
2019-03-08 22:34:46 -0500

    multiple dropdown choices and a make changes button

--- funText/funText.js
+++ funText/funText.js
@@ -4,10 +4,15 @@
 
 const userInputEl = document.getElementById('userInput');
 const changedTextEl = document.getElementById('changedText');
-const dropdownEl = document.getElementById('dropdown');
-dropdownEl.addEventListener('change', function() {
-    const userInput = userInputEl.value;
-    const dropdownValue = dropdownEl.value;
-    changedTextEl.style.color = dropdownValue;
-    changedTextEl.innerHTML = userInput;
+const colorsEl = document.getElementById('colors');
+const fontsEl = document.getElementById('fonts');
+const sizeEl = document.getElementById('size');
+
+
+const buttonEl = document.querySelector('button');
+buttonEl.addEventListener('click', function() {
+    changedTextEl.innerHTML = userInputEl.value;
+    changedTextEl.style.color = colorsEl.value;
+    changedTextEl.style.fontFamily = fontsEl.value;
+    changedTextEl.style.fontSize = sizeEl.value;
 });
