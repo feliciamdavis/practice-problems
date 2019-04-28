fcae793bc826af478ad55f71d4bbc331d88deda9
Felicia Davis <davisfeliciam@gmail.com>
2019-04-28 13:34:24 -0700

    random dice roll with img

--- diceRoll/diceRoll.js
+++ diceRoll/diceRoll.js
@@ -1,10 +1,12 @@
 /**
  * print random number 1-6 (e.g., emulate rolling a 6-sided die)
  */
+
 const buttonEl = document.querySelector('button');
-const rollEl = document.getElementById('diceNumber');
+const imgEl = document.getElementById('img');
 
 buttonEl.addEventListener('click', function () {
     const roll = Math.floor(Math.random() * 6) + 1;
-    rollEl.innerHTML = roll;
+    const img = `die${roll}`;
+    imgEl.className = img;
 });
